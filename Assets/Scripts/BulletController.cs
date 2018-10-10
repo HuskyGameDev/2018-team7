5da388746fa9c6c5e09550c2890a59f1e06aee49
Jason Bricco <jasonbricco@gmail.com>
2018-10-10 16:55:10 -0400

    -Added a BulletController class just to get the project compiling. Replace with the real one.

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+// This is a temporary fix since the required BulletController needed by other scripts was not included.
+// We need the project to compile so we can work on our parts of the project.
+// Delete this when the actual bullet controller is put in.
+public class BulletController : MonoBehaviour
+{
+	public float speedX = 0.0f;
+	public float speedY = 0.0f;
+}
