38d5802a650f503021424ca80c6905e4aa50a45b
Jason Bricco <jasonbricco@gmail.com>
2018-11-27 21:16:52 -0500

    -Fixed enemy missing the DropRate script. -Assigned a value to SMG's pellet count (not sure what the intended value is, it was left 0).

--- Assets/Scripts/SMG.cs
+++ Assets/Scripts/SMG.cs
@@ -4,9 +4,8 @@ using System.Collections.Generic;
 
 public class SMG : Gun
 {
-    public GameObject bullet;
     public float timeBetweenShots = 0.11111f;
-    public int pelletCount;
+	private int pelletCount = 2;
     public float spreadAngle;
     public float pelletFireVel = 1;
     //public GameObject pellet;
@@ -31,84 +30,65 @@ public class SMG : Gun
     }
 
     // Update is called once per frame
-    void Update() {
-        if (Time.time >= timestamp && Input.GetKey(KeyCode.RightArrow))
-        {
-            Fire();
-        }
-        else if (Time.time >= timestamp && Input.GetKey(KeyCode.LeftArrow))
-        {
-            Fire();
-        }
-        else if (Time.time >= timestamp && Input.GetKey(KeyCode.UpArrow))
-        {
-            Fire();
-        }
-        else if (Time.time >= timestamp && Input.GetKey(KeyCode.DownArrow))
-        {
-            Fire();
-        }
-    }
+    void Update()
+	{
+		int i = 0;
+		//Shoots Right
+		if (Time.time >= timestamp && Input.GetKey(KeyCode.RightArrow))
+		{
+			foreach (Quaternion quat in pellets)
+			{
+				timestamp = Time.time + timeBetweenShots;
+				pellets[i] = Random.rotation;
+				//GameObject p = Instantiate(bullet, BarrelExit.position, BarrelExit.rotation);
+				GameObject p = Instantiate(bullet, transform.position, transform.rotation); // TEMP: BarrelExit is null.
+				p.transform.rotation = Quaternion.RotateTowards(p.transform.rotation, pellets[i], spreadAngle);
+				p.GetComponent<Rigidbody2D>().AddForce(p.transform.up * pelletFireVel);
+				i++;
+			}
+		}
 
-      void Fire()
-    {
-        int i = 0;
-        //Shoots Right
-        if (Time.time >= timestamp && Input.GetKey(KeyCode.RightArrow))
-        {
-            foreach (Quaternion quat in pellets)
-            {
-                timestamp = Time.time + timeBetweenShots;
-                pellets[i] = Random.rotation;
-                GameObject p = Instantiate(bullet, BarrelExit.position, BarrelExit.rotation);
-                p.transform.rotation = Quaternion.RotateTowards(p.transform.rotation, pellets[i], spreadAngle);
-                p.GetComponent<Rigidbody2D>().AddForce(p.transform.up * pelletFireVel);
-                i++;
-            }
-        }
+		//Shoots Left
+		else if (Time.time >= timestamp && Input.GetKey(KeyCode.LeftArrow))
+		{
+			foreach (Quaternion quat in pellets)
+			{
+				timestamp = Time.time + timeBetweenShots;
+				pellets[i] = Random.rotation;
+				GameObject p = Instantiate(bullet, BarrelExit.position, BarrelExit.rotation);
+				p.transform.rotation = Quaternion.RotateTowards(p.transform.rotation, pellets[i], spreadAngle);
+				p.GetComponent<Rigidbody2D>().AddForce(p.transform.up * pelletFireVel);
+				i++;
+			}
 
-        //Shoots Left
-        else if (Time.time >= timestamp && Input.GetKey(KeyCode.LeftArrow))
-        {
-            foreach (Quaternion quat in pellets)
-            {
-                timestamp = Time.time + timeBetweenShots;
-                pellets[i] = Random.rotation;
-                GameObject p = Instantiate(bullet, BarrelExit.position, BarrelExit.rotation);
-                p.transform.rotation = Quaternion.RotateTowards(p.transform.rotation, pellets[i], spreadAngle);
-                p.GetComponent<Rigidbody2D>().AddForce(p.transform.up * pelletFireVel);
-                i++;
-            }
+		}
+		//Shoots Up
+		else if (Time.time >= timestamp && Input.GetKey(KeyCode.UpArrow))
+		{
+			foreach (Quaternion quat in pellets)
+			{
+				timestamp = Time.time + timeBetweenShots;
+				pellets[i] = Random.rotation;
+				GameObject p = Instantiate(bullet, BarrelExit.position, BarrelExit.rotation);
+				p.transform.rotation = Quaternion.RotateTowards(p.transform.rotation, pellets[i], spreadAngle);
+				p.GetComponent<Rigidbody2D>().AddForce(p.transform.up * pelletFireVel);
+				i++;
+			}
 
-        }
-        //Shoots Up
-        else if (Time.time >= timestamp && Input.GetKey(KeyCode.UpArrow))
-        {
-            foreach (Quaternion quat in pellets)
-            {
-                timestamp = Time.time + timeBetweenShots;
-                pellets[i] = Random.rotation;
-                GameObject p = Instantiate(bullet, BarrelExit.position, BarrelExit.rotation);
-                p.transform.rotation = Quaternion.RotateTowards(p.transform.rotation, pellets[i], spreadAngle);
-                p.GetComponent<Rigidbody2D>().AddForce(p.transform.up * pelletFireVel);
-                i++;
-            }
+		}
+		//Shoots Down
+		else if (Time.time >= timestamp && Input.GetKey(KeyCode.DownArrow))
+		{
+			foreach (Quaternion quat in pellets)
+			{
+				timestamp = Time.time + timeBetweenShots;
+				pellets[i] = Random.rotation;
+				GameObject p = Instantiate(bullet, BarrelExit.position, BarrelExit.rotation);
+				p.transform.rotation = Quaternion.RotateTowards(p.transform.rotation, pellets[i], spreadAngle);
+				p.GetComponent<Rigidbody2D>().AddForce(p.transform.up * pelletFireVel);
+				i++;
+			}
 
-        }
-        //Shoots Down
-        else if (Time.time >= timestamp && Input.GetKey(KeyCode.DownArrow))
-        {
-            foreach (Quaternion quat in pellets)
-            {
-                timestamp = Time.time + timeBetweenShots;
-                pellets[i] = Random.rotation;
-                GameObject p = Instantiate(bullet, BarrelExit.position, BarrelExit.rotation);
-                p.transform.rotation = Quaternion.RotateTowards(p.transform.rotation, pellets[i], spreadAngle);
-                p.GetComponent<Rigidbody2D>().AddForce(p.transform.up * pelletFireVel);
-                i++;
-            }
-
-        }
-
-    }
+		}
+	}
 }
