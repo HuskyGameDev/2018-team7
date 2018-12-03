3c9981dbf0e5b508c3d6011fd93105b758661579
Noah de Longpre <nkdelong@mtu.edu>
2018-12-02 20:50:58 -0500

    Working Leaderboard
    Leaderboard works, just need to use the Save() function from the script to save to that file, and it auto sorts and updates the leaderboard.

--- Assets/Scripts/HighscoreSaving.cs
+++ Assets/Scripts/HighscoreSaving.cs
@@ -4,23 +4,44 @@ using System.Collections.Generic;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HighscoreSaving : MonoBehaviour {
 
-    PlayerData[] LB = new PlayerData[11];
-    int LB_MAX = 10;
+
+    static int LB_MAX = 10;
+    PlayerData[] LB = new PlayerData[LB_MAX];
+
+    public Text text1;
+    public Text text2;
+    public Text text3;
+    public Text text4;
+    public Text text5; //All of the text that should be output to the Leaderboard
+    public Text text6;
+    public Text text7;
+    public Text text8;
+    public Text text9;
+    public Text text10;
+    
+   
 
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start () {
 
         Load();  //Loads the information from the file if it exists
-
-	}
+        //Save(10000, "Noah");
+        //Save(20000, "Sarah");
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		//Nothing to update, just needs to run once during load. 
+        //Nothing to update, just needs to run once during load. 
+
+
+        
+
 	}
 
 
@@ -30,36 +51,43 @@ public class HighscoreSaving : MonoBehaviour {
         BinaryFormatter bf = new BinaryFormatter(); //Honestly unsure, but it works
         FileStream file = File.Open(Application.persistentDataPath + "/highScore.dat", FileMode.OpenOrCreate); //Open or create the file if it doesn't exist
 
-        PlayerData data = new PlayerData(newScore, newName); //Save a possible new entry as PlayerData Type
+        PlayerData newData = new PlayerData(newScore, newName); //Save a possible new entry as PlayerData Type
         //data.name = newName; //Save new name
         //data.highscore = newScore; //Save new score
 
-        LeaderSort(data); //Sort the data like a Leaderboard
+        LeaderSort(newData); //Sort the data like a Leaderboard
 
-        bf.Serialize(file, data);
+        bf.Serialize(file, LB);
         file.Close();
+
+        updateScores();
     }
     
     public void Load()
     {
-        PlayerData[] data = new PlayerData[10];
+        LB = new PlayerData[10];
+
+        //Testing
+        for (int i = 0; i < LB_MAX; i++)
+        {
+            LB[i] = new PlayerData(i, "Testing" + i);
+        }
 
 
         if (File.Exists(Application.persistentDataPath + "/highScore.dat")) //If someone has played this before, and a highscore exists
         {
             BinaryFormatter bf = new BinaryFormatter();
             FileStream file = File.Open(Application.persistentDataPath + "/highScore.dat", FileMode.Open);
-            data = (PlayerData[]) bf.Deserialize(file);
+            LB = (PlayerData[]) bf.Deserialize(file);
             file.Close();
 
         }
 
+        //PlayerData fake = new PlayerData(0, "Bob");
+        //LeaderSort(fake);
+        updateScores();
 
-        //Testing
-        for(int i = 0; i < LB_MAX; i++)
-        {
-            data[i] = new PlayerData(i, "Testing");
-        }
+        
 
 
 
@@ -68,15 +96,21 @@ public class HighscoreSaving : MonoBehaviour {
     //Sort the leaderboard, and only keep the top 10 values.
     private void LeaderSort(PlayerData newPlayerData)
     {
-        PlayerData[] temp = new PlayerData[11];
+        PlayerData[] temp = new PlayerData[LB_MAX + 1];
         PlayerData swap = null;
-        temp[11] = newPlayerData; //Put it in there and sort
         
 
-        for(int i = 0; i < temp.Length; i++)
+        for(int i = 0; i < LB_MAX; i++)
+        {
+            temp[i] = LB[i];
+        }
+        temp[LB_MAX] = newPlayerData; //Put it in there and sort
+
+
+        for (int i = 0; i < temp.Length; i++)
             for(int j = 0; j < temp.Length; j++)
             {
-                if(temp[i].highscore < temp[j].highscore)
+                if(temp[i].highscore > temp[j].highscore)
                 {
                     //Swap
                     swap = temp[i];
@@ -94,6 +128,25 @@ public class HighscoreSaving : MonoBehaviour {
 
     }
 
+    void updateScores()
+    {
+        text1.text  = LB[0].toString();
+        text2.text  = LB[1].toString();
+        text3.text  = LB[2].toString();
+        text4.text  = LB[3].toString();
+        text5.text  = LB[4].toString();
+        text6.text  = LB[5].toString();
+        text7.text  = LB[6].toString();
+        text8.text  = LB[7].toString();
+        text9.text  = LB[8].toString();
+        text10.text = LB[9].toString();
+
+
+
+    }
+
+    
+
 
     [Serializable]
     class PlayerData
@@ -106,12 +159,22 @@ public class HighscoreSaving : MonoBehaviour {
 
         public long highscore;
         public string name;
+
+
+        public string toString()
+        {
+            string ans = "";
+            ans += name + " " + highscore ;
+            return ans;
+        }
+
     }
 
-    [Serializable]
-    class CurrentLeaderboard
-    {
-        PlayerData[] leaderboard;
-    }
+    //[Serializable]
+    //class CurrentLeaderboard
+    //{
+    //    public PlayerData[] leaderboard;
+    //}
+
 
 }
