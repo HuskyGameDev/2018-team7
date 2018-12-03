07c7a8e16419043121b59fbd8829ff6464523ff2
Alec Rivet <ajrivet@mtu.edu>
2018-12-03 18:46:01 -0500

    working level display thing
    and other bug fixes.

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour {
     private PlayerController playerController;
+    private Floor floor;
+
+    public Text text;
 
     // Use this for initialization
     void Start () {
         GameObject playerControllerObject = GameObject.FindWithTag("Player");
+        GameObject floorObject = GameObject.FindWithTag("Floor");
+
         if (playerControllerObject != null)
         {
             playerController = playerControllerObject.GetComponent<PlayerController>();
+            floor = floorObject.GetComponent<Floor>();
+
         }
         else
         {
@@ -22,6 +30,7 @@ public class HealthBar : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        text.text = floor.FloorID.ToString();
         float pos = ((playerController.health / 100f )*130f)-65f;
         this.transform.SetX(pos);
     }
