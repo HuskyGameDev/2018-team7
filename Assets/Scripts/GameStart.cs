aaee048030282b5831803741330b76bc491a3ace
Jason Bricco <jmbricco@mtu.edu>
2018-12-10 18:00:24 -0500

    -Forcing 1024x576 resolution on start so the resolution in the build is correct.

---
+++ Assets/Scripts/GameStart.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start()
+	{
+		Screen.SetResolution(1024, 576, false);
+	}
+}
