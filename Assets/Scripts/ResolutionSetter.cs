700a85ac698ff7bd9148f0716ff40f6cb30ed514
Jason Bricco <jasonbricco@gmail.com>
2019-02-01 21:27:35 -0500

    -Renamed GameStart to ResolutionSetter. Its job is to set the resolution to 16:9 1024x576 upon game start. This was only in the main menu scene before. I added it to the game scene too in case the main menu isn't included in the build.

---
+++ Assets/Scripts/ResolutionSetter.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class ResolutionSetter : MonoBehaviour
+{
+	void Start()
+	{
+		if (Screen.width != 1024 || Screen.height != 576)
+			Screen.SetResolution(1024, 576, false);
+	}
+}
