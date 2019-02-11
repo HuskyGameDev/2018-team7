fa3319a115c5112fa8fedcdfd6d806b999e69844
Alec Rivet <ajrivet@mtu.edu>
2019-02-11 18:09:16 -0500

    refactored ui element script
    healthbar is renamed as the script also handled other ui elements

--- Assets/Scripts/UIElements.cs
+++ Assets/Scripts/UIElements.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HealthBar : MonoBehaviour {
+public class UIElements : MonoBehaviour {
     private PlayerController playerController;
     private Floor floor;
     private Stats stats;
