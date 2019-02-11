2c8fe89bd24ee2a6562a5c6e5ba33413415f7a30
klrapa <klrapa@mtu.edu>
2019-02-11 17:48:35 -0500

    Update bossman.cs
    working boss. can move in different directions and can shoot in different directions. will be refined in the future.

--- Assets/Scripts/bossman.cs
+++ Assets/Scripts/bossman.cs
@@ -11,7 +11,7 @@ public class bossman : MonoBehaviour {
     public float speed;
     Vector3 playerPos;
 
-    public Sprite[] sprites;
+  
 
 	// Use this for initialization
 	void Start () {
@@ -63,7 +63,6 @@ public class bossman : MonoBehaviour {
             yield return new WaitForSeconds(.7f);
         }
         yield return new WaitForSeconds(.1f);
-        //transform.localScale = new Vector2(-1, 1);
         //moves up and shoots down
         ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
         //playerPos = player.transform.position;
