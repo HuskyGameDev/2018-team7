f51881177af2228552dbad3c69c5a46a874f94d6
unknown <asrospie@mtu.edu>
2019-02-27 23:40:45 -0500

    Bomber enemy that blows up when near a player

---
+++ Assets/Scripts/Enemies/Bomber.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomber : MonoBehaviour
+{
+    private PlayerController pc;
+    private float range = 5f;
+    private Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pc = GetComponent<Enemy>().pc;
+        target = pc.transform;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, target.position) <= range)
+            Explode();
+    }
+
+    private void Explode()
+    {
+        pc.health -= 50;
+        Destroy(gameObject);
+    }
+}
