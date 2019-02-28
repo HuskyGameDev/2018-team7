d247f6a743c92be0d0b0fc8a137d3a1106f88569
Jason Bricco <jasonbricco@gmail.com>
2019-02-27 20:25:10 -0500

    -Fixed compile error.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -43,8 +43,8 @@ public class GameController : MonoBehaviour
 	{
 		seed = newSeed;
 
-        // Seeds the random generator.
-        UnityEngine.Random.InitState(newSeed);
+		// Seeds the random generator.
+		UnityEngine.Random.InitState(newSeed);
 
 		seedText.text = "Seed: " + newSeed.ToString();
 	}
@@ -77,7 +77,7 @@ public class GameController : MonoBehaviour
 		pauseMenu.SetActive(false);
 		newLevelInput.SetActive(false);
 	}
-	
+
 	// Linked to the pause menu UI. This is called when the UI button is clicked, and causes the
 	// seed input UI to become active.
 	public void NewLevelButtonHandler()
@@ -122,60 +122,60 @@ public class GameController : MonoBehaviour
 		}
 	}
 
-    public void MainMenuButtonHandler()
-    {
-        Unpause();
-        SceneManager.LoadScene("MainMenu");
-    }
+	public void MainMenuButtonHandler()
+	{
+		Unpause();
+		SceneManager.LoadScene("MainMenu");
+	}
 
-    public void QuickSaveButtonHandler()
-    {
-        //TODO:populate object with relavent data
-        SaveData newSave = new SaveData(null,null,null,null) ;
-        newSave.floor = Floor.Instance.FloorID;
-        newSave.seed = seed;
+	public void QuickSaveButtonHandler()
+	{
+		//TODO:populate object with relavent data
+		SaveData newSave = new SaveData(null, null, null, null);
+		newSave.floor = Floor.Instance.FloorID;
+		newSave.seed = seed;
 
-        PlayerController pc = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
-        newSave.guns = pc.GetGunArray();
-        newSave.score = newSave.floor;
+		PlayerController pc = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+		newSave.guns = pc.GetGunArray();
+		newSave.score = newSave.floor;
 
-        BinaryFormatter bf = new BinaryFormatter(); //Honestly unsure, but it works
-        FileStream file = File.Open(Application.persistentDataPath + "/savegame.dat", FileMode.OpenOrCreate); //Open or create the file if it doesn't exist
-
-         
-
-        bf.Serialize(file, newSave); //Make it able to be pushed to the file
-        file.Close();
-
-        //updateScores(); //Update what is shown on screen in case something changed
-    }
-    //
-    public void QuickLoadButtonHandler()
-    {
-        SaveData loaded = new SaveData(null,null,null,null) ; //empty savedata to add the file info to
-
-        if (File.Exists(Application.persistentDataPath + "/highScore.dat")) //If someone has played this before, and a highscore exists
-        {
-            BinaryFormatter bf = new BinaryFormatter();  //Open new formatter
-            FileStream file = File.Open(Application.persistentDataPath + "/SaveGame.dat", FileMode.Open); //Open the existing file
-            loaded = (SaveData)bf.Deserialize(file); //Make the file readable to me again
-            file.Close(); //Close the file because I read from it already. 
-
-            seed = loaded.seed??0;//set seed to the loaded seed, if loaded one is null somehow set it to 0
-
-            Floor.Instance.FloorID = loaded.floor ?? 1;
-
-            PlayerController pc = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
-            pc.SetGunArray(loaded.guns);
+		BinaryFormatter bf = new BinaryFormatter(); //Honestly unsure, but it works
+		FileStream file = File.Open(Application.persistentDataPath + "/savegame.dat", FileMode.OpenOrCreate); //Open or create the file if it doesn't exist
 
 
-            //Time.timeScale = 1.0f;
-            //SceneManager.LoadScene("Game");
-            return;
+
+		bf.Serialize(file, newSave); //Make it able to be pushed to the file
+		file.Close();
+
+		//updateScores(); //Update what is shown on screen in case something changed
+	}
+	//
+	public void QuickLoadButtonHandler()
+	{
+		SaveData loaded = new SaveData(null, null, null, null); //empty savedata to add the file info to
+
+		if (File.Exists(Application.persistentDataPath + "/highScore.dat")) //If someone has played this before, and a highscore exists
+		{
+			BinaryFormatter bf = new BinaryFormatter();  //Open new formatter
+			FileStream file = File.Open(Application.persistentDataPath + "/SaveGame.dat", FileMode.Open); //Open the existing file
+			loaded = (SaveData)bf.Deserialize(file); //Make the file readable to me again
+			file.Close(); //Close the file because I read from it already. 
+
+			seed = loaded.seed ?? 0;//set seed to the loaded seed, if loaded one is null somehow set it to 0
+
+			Floor.Instance.FloorID = loaded.floor ?? 1;
+
+			PlayerController pc = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+			pc.SetGunArray(loaded.guns);
 
 
-        }
-    }
+			//Time.timeScale = 1.0f;
+			//SceneManager.LoadScene("Game");
+			return;
+
+
+		}
+	}
 
 
 	private void Update()
@@ -188,29 +188,24 @@ public class GameController : MonoBehaviour
 		}
 	}
 
-    [Serializable]
-    class SaveData
-    {
-        //Private class to hold data about player
-        public SaveData(int? seed, int? floor, int? score, bool[] guns)
-        {
-            this.seed = seed;
-            this.floor = floor;
-            this.score = score;
-            this.guns = guns;
-        }
+	[Serializable]
+	class SaveData
+	{
+		//Private class to hold data about player
+		public SaveData(int? seed, int? floor, int? score, bool[] guns)
+		{
+			this.seed = seed;
+			this.floor = floor;
+			this.score = score;
+			this.guns = guns;
+		}
 
-        public int? seed;
-        public int? score;
-        public int? floor;
-        public bool[] guns;
+		public int? seed;
+		public int? score;
+		public int? floor;
+		public bool[] guns;
+	}
 
-
-
-    }
-   }
-
- 
 	private void OnDestroy()
 	{
 		PlayerPrefs.SetInt("Score", score);
