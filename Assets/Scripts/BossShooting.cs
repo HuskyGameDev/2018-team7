46fa2c4381d4f0eb65018312d8c6c2d100ce292f
klrapa <klrapa@mtu.edu>
2019-02-28 19:19:54 -0500

    DO NOT TOUCH THESE YET.
    I'm working on getting these together but for now I need to build them separately for the three different phases.

---
+++ Assets/Scripts/BossShooting.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShooting : MonoBehaviour
+{
+    public Vector3 bulletSet = new Vector3(0, 0.5f, 0);
+    public GameObject bulletPrefab;
+
+    public float fireDelay = 0.50f;
+    float cooldownTimer = 0;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        cooldownTimer -= Time.deltaTime;
+        if (Input.GetButton("Fire1") && cooldownTimer <= 0)
+        {
+            Debug.Log("Pew!");
+            cooldownTimer = fireDelay;
+
+            Vector3 offset = transform.rotation * bulletSet;
+            GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, transform.position + offset, transform.rotation);
+            bulletGO.layer = gameObject.layer;
+        }
+    }
+}
--- Assets/Scripts/bossman.cs
+++ Assets/Scripts/bossman.cs
@@ -4,87 +4,108 @@ using UnityEngine;
 
 public class bossman : MonoBehaviour
 {
+    //FIRST PHASE
+    float health = 100;
+    public Transform[] bossPos;
+    public Transform[] holes;
+    public GameObject rocket;
+    GameObject player;
+    public float speed;
+    Vector3 playerPos;
+    //SECOND PHASE
+    //public Transform player;
+    public Transform myTransform;
+    public float maxSpeed;
+    //THIRD PHASE
 
-	public Transform[] bossPos;
-	public Transform[] holes;
-	public GameObject rocket;
-	GameObject player;
-	public float speed;
-	Vector3 playerPos;
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("player");
+        StartCoroutine("Boss");
+
+        //transform.position = waypoints[waypointIndex].transform.position;
 
 
+    }
 
-	// Use this for initialization
-	void Start()
-	{
-		player = GameObject.FindGameObjectWithTag("player");
-		StartCoroutine("Boss");
-	}
+    // Update is called once per frame
+    void Update()
+    {
+       // while (true)
+        {
+            //PHASE THREE
+           // if (health <= 10)
+            {
+              
 
-	// Update is called once per frame
-	void Update()
-	{
+            }
+            //PHASE TWO
+           // else if (health <= 50 && health > 10)
+            {
+               // transform.LookAt(player);
+                //transform.Translate(Vector3.forward * maxSpeed * Time.deltaTime);
+            }
+        }
 
-	}
+    }
+    //PHASE ONE
+    IEnumerator Boss()
+    {
 
-	IEnumerator Boss()
-	{
-		//working
-		//moves right and shoots left
-		//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		while (transform.position.x != bossPos[0].position.x)
-		{
-			transform.position = Vector2.MoveTowards(transform.position, new Vector2(bossPos[0].position.x, transform.position.y), speed);
-			yield return null;
+       //while (health <= 100 && health > 50) 
+       // {
+            //FIRST ATTACK
+            //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            while (transform.position.x != bossPos[0].position.x)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, new Vector2(bossPos[0].position.x, transform.position.y), speed);
+                yield return null;
 
-		}
-		transform.localScale = new Vector2(-1, 1);
-		yield return new WaitForSeconds(.1f);
-		int b = 0;
-		while (b < 4)
-		{
-			GameObject rock = (GameObject)Instantiate(rocket, holes[Random.Range(0, 2)].position, Quaternion.identity);
-			rock.GetComponent<Rigidbody2D>().velocity = Vector2.left * 5;
-			b++;
-			yield return new WaitForSeconds(.7f);
-		}
-		//moves left and shoots right
-		///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		GetComponent<Rigidbody2D>().isKinematic = true;
-		while (transform.position.x != bossPos[2].position.x)
-		{
-			transform.position = Vector2.MoveTowards(transform.position, new Vector2(bossPos[2].position.x, transform.position.y), speed);
-			yield return null;
+            }
+            transform.localScale = new Vector2(-1, 1);
+            yield return new WaitForSeconds(.1f);
+            int b = 0;
+            while (b < 2)
+            {
+                GameObject rock = (GameObject)Instantiate(rocket, holes[Random.Range(0, 2)].position, Quaternion.identity);
+                rock.GetComponent<Rigidbody2D>().velocity = Vector2.left * 5;
+                b++;
+                yield return new WaitForSeconds(.7f);
+            }
+            //SECOND ATTACK
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            //GetComponent<Rigidbody2D>().isKinematic = true;
+            yield return new WaitForSeconds(.1f);
 
-		}
-		int x = 0;
-		while (x < 4)
-		{
-			GameObject rock = (GameObject)Instantiate(rocket, holes[Random.Range(0, 2)].position, Quaternion.identity);
-			rock.GetComponent<Rigidbody2D>().velocity = Vector2.right * 5;
-			x++;
-			yield return new WaitForSeconds(.7f);
-		}
-		yield return new WaitForSeconds(.1f);
-		//moves up and shoots down
-		///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		//playerPos = player.transform.position;
-		yield return new WaitForSeconds(.1f);
-		while (transform.position != bossPos[1].position)
-		{
-			transform.position = Vector2.MoveTowards(transform.position, bossPos[1].position, speed);
-			yield return null;
-		}
-		int y = 0;
-		while (y < 4)
-		{
-			GameObject rock = (GameObject)Instantiate(rocket, holes[Random.Range(0, 2)].position, Quaternion.identity);
-			rock.GetComponent<Rigidbody2D>().velocity = Vector2.down * 5;
-			y++;
-			yield return new WaitForSeconds(.7f);
-		}
-		yield return new WaitForSeconds(.1f);
-		////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		//GetComponent<Rigidbody2D>().isKinematic = true;
-	}
-}
+            while (transform.position.x != bossPos[2].position.x)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, bossPos[2].position, speed);
+                yield return null;
+
+            }
+
+            int x = 0;
+            while (x < 2)
+            {
+                GameObject rock = (GameObject)Instantiate(rocket, holes[Random.Range(0, 2)].position, Quaternion.identity);
+                rock.GetComponent<Rigidbody2D>().velocity = Vector2.right * 5;
+                x++;
+                yield return new WaitForSeconds(.7f);
+            }
+            //THIRD ATTACK
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            //GetComponent<Rigidbody2D>().isKinematic = false;
+            yield return new WaitForSeconds(.1f);
+            while (transform.position != bossPos[1].position)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, bossPos[1].position, speed);
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(.7f);
+
+        }
+    }
+//}
+
