b205690fed43a9bd1f97b97ebaa75e7fda5f33e1
klrapa <klrapa@mtu.edu>
2019-03-28 21:18:11 -0500

    BOSS UPDATE: Just some updates removing stuff I dont need
    boi

---
+++ Assets/Scripts/BossAssets/MoveBullet.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBullet : MonoBehaviour
+{
+
+    float maxSpeed = 20f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        Vector3 velocity = new Vector3(0, maxSpeed * Time.deltaTime, 0);
+
+        pos += transform.rotation * velocity;
+
+        transform.position = pos;
+
+    }
+}
---
+++ Assets/Scripts/BossAssets/PlayerHurt3.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHurt3 : MonoBehaviour {
+
+    public int damageToGive;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<PlayerHealthLevel4>().HurtEnemy(damageToGive);
+        }
+
+    }
+}
